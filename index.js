8af8fc2a8fb8e9808ca5a2dc866bc452dad7494c
Jasmine Carter <jasmine.carter87@gmail.com>
2020-11-29 23:57:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let event = 'Surprise'
 
 
 function writeCards(names, event) {
-
+  let thankYouCards = []
   for (let i = 0; i < names.length; i++) {
     console.log(`Thank you,${names[i]}, for the wonderful ${event} gift!`);
   }
