54e26f99ffc9c60ec12c26bfe99e6061084741a7
Jasmine Carter <jasmine.carter87@gmail.com>
2020-11-29 23:57:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Code your solutions in this file
 const names =['Lisa', 'Kaitlin', 'Jan']
+let event = 'Surprise'
+
 
 function writeCards(names) {
 
   for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you,${names[i]}, for the wonderful surprise gift!`);
+    console.log(`Thank you,${names[i]}, for the wonderful ${event} gift!`);
   }
   return names;
 }
 
-writeCards(names);
+writeCards(names, );
