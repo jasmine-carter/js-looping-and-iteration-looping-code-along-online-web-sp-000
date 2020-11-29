bec8d3692eab7f715ac6866e6a3f567afe6dfe50
Jasmine Carter <jasmine.carter87@gmail.com>
2020-11-29 23:54:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ function writeCards(names) {
   for (let i = 0; i < names.length; i++) {
     console.log(`Thank you, ${names[i]}, for the wonderful surprise gift`);
   }
+  return names;
 }
+
+writeCards(names);
